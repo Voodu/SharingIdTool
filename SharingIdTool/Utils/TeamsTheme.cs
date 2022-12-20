64f850b595c7aba79c8c898dfa3ee0bdfddc190b
Piotr Ładoński <piolado@gmail.com>
2022-12-20 17:22:17 +0100

    Use Teams colors & themes

---
+++ SharingIdTool/Utils/TeamsTheme.cs
@@ -0,0 +1,8 @@
+﻿namespace SharingIdTool.Utils;
+
+public static class TeamsTheme
+{
+    public const string Default = "default";
+    public const string Dark = "dark";
+    public const string Contrast = "contrast";
+}
